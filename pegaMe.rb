991ba619e7c0a533d5b715a785ad52f4e97157d0
Josue moura <josuepaulista@hotmail.com>
2014-03-18 03:10:22 -0300

    Maquina formatada

--- pegaMe.rb
+++ pegaMe.rb
@@ -3,3 +3,5 @@
 
 puts 'Arquivo editado sobre o terminal do github'
 puts 'Alteração feita na minha maquina'
+puts 'Maquina formatada'
+puts 'Montando script para automatizar instalacao de programas importantes'
